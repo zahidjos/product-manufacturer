f6982de9c0e8dbb41bc1b9725aab0d53fb5a09df
zahidjos <zahidhasanjos90@gmail.com>
2024-10-06 11:34:11 +0600

    banner update

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Banner.css';
 import banner1 from '../../image/banner (1).jpg'
 import banner2 from '../../image/banner (2).jpg'
 import banner3 from '../../image/banner (3).jpg'
@@ -6,10 +7,10 @@ import banner3 from '../../image/banner (3).jpg'
 const Banner = () => {
     return (
        
-<div>
+<div className='banner'>
 <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src="https://api.lorem.space/image/car?w=800&h=200&hash=8B7BCDC2" alt='no image' className="w-full"/> 
+    <img src="https://i.ibb.co/g4nK7J3/banner-2.jpg" alt='no image' className="w-full"/> 
     <div className="absolute text-center transform -translate-y-1/2 left-5 right-5  bottom-1/4">
       <h1 className='text-white text-5xl font-bold'>Welcome to our</h1>
       <h5 className='text-amber-600 text-3xl mt-3 font-medium'>  Parts Manufacturing site</h5>
@@ -20,7 +21,7 @@ const Banner = () => {
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src="https://api.lorem.space/image/car?w=800&h=200&hash=500B67FB" className="w-full"/> 
+    <img src="https://i.ibb.co/vqHR4RG/banner-3.jpg" className="w-full"/> 
     <div className="absolute text-center transform -translate-y-1/2 left-5 right-5  bottom-1/4">
     <h1 className='text-white text-5xl font-bold'>Welcome to our</h1>
       <h5 className='text-amber-600 text-3xl mt-3 font-medium'>  Parts Manufacturing site</h5>
@@ -32,7 +33,7 @@ const Banner = () => {
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <img src="https://api.lorem.space/image/car?w=800&h=200&hash=A89D0DE6" className="w-full"/> 
+    <img src="https://i.ibb.co/HYKtbdM/banner-1.jpg" className="w-full"/> 
     <div className="absolute text-center transform -translate-y-1/2 left-5 right-5  bottom-1/4">
     <h1 className='text-white text-5xl font-bold'>Welcome to our</h1>
       <h5 className='text-amber-600 text-3xl mt-3 font-medium'>  Parts Manufacturing site</h5>
@@ -43,7 +44,7 @@ const Banner = () => {
     </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-    <img src="https://www.pexels.com/photo/blue-bmw-sedan-near-green-lawn-grass-170811/" className="w-full"/> 
+    <img src="https://i.ibb.co/g4nK7J3/banner-2.jpg" className="w-full"/> 
     <div className="absolute text-center transform -translate-y-1/2 left-5 right-5  bottom-1/4" >
     <h1 className='text-white text-5xl font-bold'>Welcome to our</h1>
       <h5 className='text-amber-600 text-3xl mt-3 font-medium'>  Parts Manufacturing site</h5>
